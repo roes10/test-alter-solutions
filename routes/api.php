55bb698978ad00caf2e4e0502736ccebe14d743d
Ramon Oestreich <ramon.oestreich@gmail.com>
2022-05-08 21:51:20 -0300

    routes

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,34 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['prefix' => 'products'], function (){
-    Route::get('/',[ProductController::class,'index']);
-    Route::get('/{id}',[ProductController::class,'show']);
-    Route::post('/',[ProductController::class,'store']);
-    Route::put('/{id}',[ProductController::class,'update']);
-    Route::patch('/{id}',[ProductController::class,'update']);
-    Route::delete('/{id}',[ProductController::class,'destroy']);
-});
 
+Route::group(['prefix' => 'auth', 'middleware' => 'api'], function () {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::post('refresh', [AuthController::class, 'refresh']);
+    Route::post('me', [AuthController::class, 'me']);
+});
+Route::group(['middleware' => 'jwt.auth'], function () {
+
+    Route::group(['prefix' => 'products'], function () {
+        Route::get('/', [ProductController::class, 'index']);
+        Route::get('/{id}', [ProductController::class, 'show']);
+        Route::post('/', [ProductController::class, 'store']);
+        Route::put('/{id}', [ProductController::class, 'update']);
+        Route::patch('/{id}', [ProductController::class, 'update']);
+        Route::delete('/{id}', [ProductController::class, 'destroy']);
+    });
+
+    Route::group(['prefix' => 'categories'], function () {
+        Route::get('/', [CategoryController::class, 'index']);
+        Route::get('/{id}', [CategoryController::class, 'show']);
+        Route::post('/', [CategoryController::class, 'store']);
+        Route::put('/{id}', [CategoryController::class, 'update']);
+        Route::patch('/{id}', [CategoryController::class, 'update']);
+        Route::delete('/{id}', [CategoryController::class, 'destroy']);
+    });
+});
